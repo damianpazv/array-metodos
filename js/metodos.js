8fdc644e6de7f5ee6c6c6884d333c2380c256173
damianpazv <damjpv@gmail.com>
2023-05-29 17:30:56 -0300

    agrego localstorage

--- js/metodos.js
+++ js/metodos.js
@@ -1,97 +1,99 @@
-const paisesLatinoamerica = [
-    {
-      nombre: "Argentina",
-      ubicacion: "Sur de Sudamérica",
-      habitantes: 45000000,
-      capital: "Buenos Aires",
-      imagen: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg",
-      continente: "America",
-    },
-    {
-      nombre: "Bolivia",
-      ubicacion: "Centro de Sudamérica",
-      habitantes: 11000000,
-      capital: "La Paz"
-    },
-    {
-      nombre: "Brasil",
-      ubicacion: "Este de Sudamérica",
-      habitantes: 211000000,
-      capital: "Brasilia"
-    },
-    {
-      nombre: "Chile",
-      ubicacion: "Sur de Sudamérica",
-      habitantes: 19000000,
-      capital: "Santiago"
-    },
-    {
-      nombre: "Colombia",
-      ubicacion: "Norte de Sudamérica",
-      habitantes: 50340000,
-      capital: "Bogotá"
-    },
-    {
-      nombre: "Costa Rica",
-      ubicacion: "Centroamérica",
-      habitantes: 5100000,
-      capital: "San José"
-    },
-    {
-      nombre: "Cuba",
-      ubicacion: "El Caribe",
-      habitantes: 11400000,
-      capital: "La Habana"
-    },
-    {
-      nombre: "Ecuador",
-      ubicacion: "Oeste de Sudamérica",
-      habitantes: 17300000,
-      capital: "Quito"
-    },
-    {
-      nombre: "El Salvador",
-      ubicacion: "Centroamérica",
-      habitantes: 6500000,
-      capital: "San Salvador"
-    },
-    {
-      nombre: "Guatemala",
-      ubicacion: "Centroamérica",
-      habitantes: 18200000,
-      capital: "Ciudad de Guatemala"
-    },
-    {
-      nombre: "Honduras",
-      ubicacion: "Centroamérica",
-      habitantes: 10000000,
-      capital: "Tegucigalpa"
-    },
-    {
-      nombre: "México",
-      ubicacion: "Norte de América Central",
-      habitantes: 128900000,
-      capital: "Ciudad de México"
-    },
-    {
-      nombre: "Nicaragua",
-      ubicacion: "Centroamérica",
-      habitantes: 6400000,
-      capital: "Managua"
-    },
-    {
-      nombre: "Panamá",
-      ubicacion: "Centroamérica",
-      habitantes: 4300000,
-      capital: "Ciudad de Panamá"
-    },
-    {
-      nombre: "Paraguay",
-      ubicacion: "Centro de Sudamérica",
-      habitantes: 7200000,
-      capital: "Asunción"
-    },
-  ]
+// const paisesLatinoamerica = [
+//     {
+//       nombre: "Argentina",
+//       ubicacion: "Sur de Sudamérica",
+//       habitantes: 45000000,
+//       capital: "Buenos Aires",
+//       imagen: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg",
+//       continente: "America",
+//     },
+//     {
+//       nombre: "Bolivia",
+//       ubicacion: "Centro de Sudamérica",
+//       habitantes: 11000000,
+//       capital: "La Paz"
+//     },
+//     {
+//       nombre: "Brasil",
+//       ubicacion: "Este de Sudamérica",
+//       habitantes: 211000000,
+//       capital: "Brasilia"
+//     },
+//     {
+//       nombre: "Chile",
+//       ubicacion: "Sur de Sudamérica",
+//       habitantes: 19000000,
+//       capital: "Santiago"
+//     },
+//     {
+//       nombre: "Colombia",
+//       ubicacion: "Norte de Sudamérica",
+//       habitantes: 50340000,
+//       capital: "Bogotá"
+//     },
+//     {
+//       nombre: "Costa Rica",
+//       ubicacion: "Centroamérica",
+//       habitantes: 5100000,
+//       capital: "San José"
+//     },
+//     {
+//       nombre: "Cuba",
+//       ubicacion: "El Caribe",
+//       habitantes: 11400000,
+//       capital: "La Habana"
+//     },
+//     {
+//       nombre: "Ecuador",
+//       ubicacion: "Oeste de Sudamérica",
+//       habitantes: 17300000,
+//       capital: "Quito"
+//     },
+//     {
+//       nombre: "El Salvador",
+//       ubicacion: "Centroamérica",
+//       habitantes: 6500000,
+//       capital: "San Salvador"
+//     },
+//     {
+//       nombre: "Guatemala",
+//       ubicacion: "Centroamérica",
+//       habitantes: 18200000,
+//       capital: "Ciudad de Guatemala"
+//     },
+//     {
+//       nombre: "Honduras",
+//       ubicacion: "Centroamérica",
+//       habitantes: 10000000,
+//       capital: "Tegucigalpa"
+//     },
+//     {
+//       nombre: "México",
+//       ubicacion: "Norte de América Central",
+//       habitantes: 128900000,
+//       capital: "Ciudad de México"
+//     },
+//     {
+//       nombre: "Nicaragua",
+//       ubicacion: "Centroamérica",
+//       habitantes: 6400000,
+//       capital: "Managua"
+//     },
+//     {
+//       nombre: "Panamá",
+//       ubicacion: "Centroamérica",
+//       habitantes: 4300000,
+//       capital: "Ciudad de Panamá"
+//     },
+//     {
+//       nombre: "Paraguay",
+//       ubicacion: "Centro de Sudamérica",
+//       habitantes: 7200000,
+//       capital: "Asunción"
+//     },
+//   ]
+
+const paisesLatinoamerica= JSON.parse(localStorage.getItem("paises"))  || [];
 
 let copiaArray=[...paisesLatinoamerica];
 
@@ -152,9 +154,13 @@ poblacionTotal(paisesFiltrados);
 function borrarPais(indice)
 {
   
-  copiaArray.splice(indice,1);
-  renderizarTabla(copiaArray);
-  poblacionTotal(copiaArray);
+  // copiaArray.splice(indice,1);
+  // renderizarTabla(copiaArray);
+  // poblacionTotal(copiaArray);
+
+  paisesLatinoamerica.splice(indice,1);
+  localStorage.setItem("paises",JSON.stringify());
+  renderizarTabla(paisesLatinoamerica);
 }
 
 
@@ -192,7 +198,13 @@ const el= evt.target.elements;
 
   }
 paisesLatinoamerica.push(nuevoPais);
+
+localStorage.setItem("paises",JSON.stringify(paisesLatinoamerica));
+
 renderizarTabla(paisesLatinoamerica);
+
+evt.target.reset()
+el.nombre.focus()
 })
 
 
